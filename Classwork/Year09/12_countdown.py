81ae9a2cffc1c3f762f72c88e5ee8a7692752819
Oliver <oliverng2512@icloud.com>
2024-10-01 20:18:44 +0900

    Upload C12 and C21

---
+++ Classwork/Year09/12_countdown.py
@@ -0,0 +1,3 @@
+start_number = int(input("Number to start at: "))
+for i in range(start_number, -1, -1):
+    print(i)
---
+++ Classwork/Year09/21_username.py
@@ -0,0 +1,5 @@
+first_name = input("First name: ")
+surname = input("Surname: ")
+initial = first_name[0].upper()
+username = surname + "." + initial
+print(f"Your username is: {username}")
